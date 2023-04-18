7482dc012e7382cd55831acb9d298cf3bda705a5
Morgane Porchet <morgane.porchet@hes-so.ch>
2023-04-18 11:48:17 +0200

    MAK receiver : send token frame to macSender

--- mac_receiver.c
+++ mac_receiver.c
@@ -5,7 +5,54 @@
 
 #include "main.h"
 
+osStatus_t sendTokenFrame(void * dataFramePointer)
+{
+	struct queueMsg_t queueMsg = {0};		// initialized to 0
+	osStatus_t retCode;
+	
+	// set the type and token frame pointer
+	queueMsg.type = TOKEN;
+	queueMsg.anyPtr = dataFramePointer;
+	
+	// put the message on the queue_macS
+	retCode = osMessageQueuePut(
+		queue_macS_id,
+		&queueMsg,
+		osPriorityNormal, 
+		osWaitForever);
+	
+	return retCode;
+}
+
 void MacReceiver(void *argument)
 {
-	// TODO
+	struct queueMsg_t queueMsg;
+	uint8_t * qPtr;
+	
+	osStatus_t retCode;
+	
+	for (;;)
+	{
+		
+		// read Queue
+		retCode = osMessageQueueGet(
+			queue_macR_id,
+			&queueMsg,
+			NULL,
+			osWaitForever);
+		
+		CheckRetCode(retCode, __LINE__, __FILE__, CONTINUE);
+		
+		qPtr = queueMsg.anyPtr;
+		
+		if(queueMsg.type == FROM_PHY)
+		{
+			// it's a TOKEN 
+			if (qPtr[0] == TOKEN_TAG)
+			{
+				retCode = sendTokenFrame(queueMsg.anyPtr);
+				CheckRetCode(retCode, __LINE__, __FILE__, CONTINUE);
+			}
+		}
+	}
 }
--- mac_sender.c
+++ mac_sender.c
@@ -8,28 +8,31 @@
 /*
 * 	Create the frame for the token
 */
-osStatus_t createTokenFrame(struct queueMsg_t queueMsg){
+osStatus_t createTokenFrame(void){
+	struct queueMsg_t queueMsg;
 	osStatus_t retCode;
 	uint8_t * msg;
 	
-	uint8_t msgToSend[17];
-	
 	// fill the message to send
+	uint8_t msgToSend[TOKENSIZE-2];		// TOKENSIZE(in PHY layer) - 2 to have the size in MAC layer
 	msgToSend[0] = TOKEN_TAG;
 	// State st_i fields
 	for (uint8_t i = 1; i < sizeof(msgToSend); i++)
 	{
 		msgToSend[i] = 0; 
 	}
-	msgToSend[gTokenInterface.myAddress] =  0xA;		// TODO: replace the harcoded chat application bit
+	msgToSend[MYADDRESS] =  0xA;		// TODO: replace the harcoded chat application bit
 	
 	// memory allocation
 	msg = osMemoryPoolAlloc(memPool, osWaitForever);
 	
+	// set the type and data frame pointer
 	queueMsg.type = TO_PHY;
 	queueMsg.anyPtr = msg;
+	// update content of the data frame pointer
 	memcpy(msg, msgToSend, sizeof(msgToSend));
 	
+	// put the token in the queue_phyS_id
 	retCode = osMessageQueuePut(
 		queue_phyS_id,
 		&queueMsg,
@@ -61,8 +64,13 @@ void MacSender(void *argument)
 		switch(queueMsg.type)
 		{
 			case NEW_TOKEN:
-				retCode = createTokenFrame(queueMsg);
+				retCode = createTokenFrame();
 				break;
 		}
+		
+		CheckRetCode(retCode, __LINE__, __FILE__, CONTINUE);
+		
+		
+		// TODO : memory release
 	}
 }
